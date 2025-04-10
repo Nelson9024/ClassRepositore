a9c8556963689798c81e61d254fa5bd20b029dbf
Nelson9466 <naguilardavid@gmail.com>
2025-04-10 18:49:59 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Función para mostrar mensajes
+const showMessage = (message) => message;
+
+// Mostrar mensaje
+console.log(showMessage("Hola cabeza de bola"));
+console.log("Hola marx cruz");
+
+// Variables y suma
+var a = 7;
+var b = 10;
+
+var r = a + b;
+
+// Mostrar resultado con interpolación de cadenas
+console.log(`El resultado es: ${r}`);
+console.log("Ya lo enlace");
